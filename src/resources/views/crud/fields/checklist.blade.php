70dcb8af9064985034f6dad79f2dc132b107367f
hacoidev <hacoi.dev@gmail.com>
2022-07-29 14:48:56 +0700

    update fields

--- src/resources/views/crud/fields/checklist.blade.php
+++ src/resources/views/crud/fields/checklist.blade.php
@@ -1,61 +1,64 @@
 <!-- checklist -->
 @php
-  $key_attribute = (new $field['model'])->getKeyName();
-  $field['attribute'] = $field['attribute'] ?? (new $field['model'])->identifiableAttribute();
-  $field['number_of_columns'] = $field['number_of_columns'] ?? 3;
+$key_attribute = (new $field['model']())->getKeyName();
+$field['attribute'] = $field['attribute'] ?? (new $field['model']())->identifiableAttribute();
+$field['number_of_columns'] = $field['number_of_columns'] ?? 3;
 
-  // calculate the checklist options
-  if (!isset($field['options'])) {
-      $field['options'] = $field['model']::all()->pluck($field['attribute'], $key_attribute)->toArray();
-  } else {
-      $field['options'] = call_user_func($field['options'], $field['model']::query());
-  }
+// calculate the checklist options
+if (!isset($field['options'])) {
+    $field['options'] = $field['model']
+        ::all()
+        ->pluck($field['attribute'], $key_attribute)
+        ->toArray();
+} else {
+    $field['options'] = call_user_func($field['options'], $field['model']::query());
+}
 
-  // calculate the value of the hidden input
-  $field['value'] = old_empty_or_null($field['name'], []) ??  $field['value'] ?? $field['default'] ?? [];
-  if(!empty($field['value'])) {
-      if (is_a($field['value'], \Illuminate\Support\Collection::class)) {
-          $field['value'] = ($field['value'])->pluck($key_attribute)->toArray();
-      } elseif (is_string($field['value'])){
+// calculate the value of the hidden input
+$field['value'] = old_empty_or_null($field['name'], []) ?? ($field['value'] ?? ($field['default'] ?? []));
+if (!empty($field['value'])) {
+    if (is_a($field['value'], \Illuminate\Support\Collection::class)) {
+        $field['value'] = $field['value']->pluck($key_attribute)->toArray();
+    } elseif (is_string($field['value'])) {
         $field['value'] = json_decode($field['value']);
-      }
-  }
+    }
+}
 
-  // define the init-function on the wrapper
-  $field['wrapper']['data-init-function'] =  $field['wrapper']['data-init-function'] ?? 'bpFieldInitChecklist';
+// define the init-function on the wrapper
+$field['wrapper']['data-init-function'] = $field['wrapper']['data-init-function'] ?? 'bpFieldInitChecklist';
 @endphp
 
 @include('crud::fields.inc.wrapper_start')
-    <label>{!! $field['label'] !!}</label>
-    @include('crud::fields.inc.translatable_icon')
+<label>{!! $field['label'] !!}</label>
+@include('crud::fields.inc.translatable_icon')
 
-    <input type="hidden" value='@json($field['value'])' name="{{ $field['name'] }}">
+<input type="hidden" value='@json($field['value'])'>
 
-    <div class="row">
-        @foreach ($field['options'] as $key => $option)
-            <div class="col-sm-{{ intval(12/$field['number_of_columns']) }}">
-                <div class="checkbox">
-                  <label class="font-weight-normal">
-                    <input type="checkbox" value="{{ $key }}"> {{ $option }}
-                  </label>
-                </div>
+<div class="row">
+    @foreach ($field['options'] as $key => $option)
+        <div class="col-sm-{{ intval(12 / $field['number_of_columns']) }}">
+            <div class="checkbox">
+                <label class="font-weight-normal">
+                    <input type="checkbox" value="{{ $key }}" name="{{ $field['name'] }}[]"> {{ $option }}
+                </label>
             </div>
-        @endforeach
-    </div>
+        </div>
+    @endforeach
+</div>
 
-    {{-- HINT --}}
-    @if (isset($field['hint']))
-        <p class="help-block">{!! $field['hint'] !!}</p>
-    @endif
+{{-- HINT --}}
+@if (isset($field['hint']))
+    <p class="help-block">{!! $field['hint'] !!}</p>
+@endif
 @include('crud::fields.inc.wrapper_end')
 
 
 {{-- ########################################## --}}
 {{-- Extra CSS and JS for this particular field --}}
 {{-- If a field type is shown multiple times on a form, the CSS and JS will only be loaded once --}}
-    {{-- FIELD JS - will be loaded in the after_scripts section --}}
-    @push('crud_fields_scripts')
-        @loadOnce('bpFieldInitChecklist')
+{{-- FIELD JS - will be loaded in the after_scripts section --}}
+@push('crud_fields_scripts')
+    @loadOnce('bpFieldInitChecklist')
         <script>
             function bpFieldInitChecklist(element) {
                 var hidden_input = element.find('input[type=hidden]');
@@ -65,34 +68,34 @@
 
                 // set the default checked/unchecked states on checklist options
                 checkboxes.each(function(key, option) {
-                  var id = $(this).val();
+                    var id = $(this).val();
 
-                  if (selected_options.map(String).includes(id)) {
-                    $(this).prop('checked', 'checked');
-                  } else {
-                    $(this).prop('checked', false);
-                  }
+                    if (selected_options.map(String).includes(id)) {
+                        $(this).prop('checked', 'checked');
+                    } else {
+                        $(this).prop('checked', false);
+                    }
                 });
 
                 // when a checkbox is clicked
                 // set the correct value on the hidden input
                 checkboxes.click(function() {
-                  var newValue = [];
+                    var newValue = [];
 
-                  checkboxes.each(function() {
-                    if ($(this).is(':checked')) {
-                      var id = $(this).val();
-                      newValue.push(id);
-                    }
-                  });
+                    checkboxes.each(function() {
+                        if ($(this).is(':checked')) {
+                            var id = $(this).val();
+                            newValue.push(id);
+                        }
+                    });
 
-                  hidden_input.val(JSON.stringify(newValue)).trigger('change');
+                    hidden_input.val(JSON.stringify(newValue)).trigger('change');
 
                 });
 
                 hidden_input.on('CrudField:disable', function(e) {
-                      checkboxes.attr('disabled', 'disabled');
-                  });
+                    checkboxes.attr('disabled', 'disabled');
+                });
 
                 hidden_input.on('CrudField:enable', function(e) {
                     checkboxes.removeAttr('disabled');
@@ -100,7 +103,7 @@
 
             }
         </script>
-        @endLoadOnce
-    @endpush
+    @endLoadOnce
+@endpush
 {{-- End of Extra CSS and JS --}}
 {{-- ########################################## --}}
